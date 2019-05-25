7482b280896c274cc35b09b9da602d4add1ed9d3
Niklas Olsson <niklasolsson94@gmail.com>
2019-05-25 14:54:04 +0100

    Initialized repository

---
+++ src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+
+public class Controller {
+    
+    @FXML
+    private Text title;
+    @FXML
+    private Text playerHand;
+    @FXML
+    private Text oppHand;
+    
+    @FXML
+    public void processAction(){
+
+    }
+}

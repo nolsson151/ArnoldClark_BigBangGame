13cb27c238e365152ad2350f982038f16b5a82a4
Niklas Olsson <niklasolsson94@gmail.com>
2019-05-25 16:19:25 +0100

    Added in winMatrix and HashMap of player hands

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,18 +3,38 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
+import java.util.HashMap;
+
 
 public class Controller {
-    
     @FXML
     private Text title;
     @FXML
     private Text playerHand;
     @FXML
     private Text oppHand;
+    private HashMap handToInt;
+    private int[][] winMatrix;
+
+    public Controller(){
+        winMatrix = new int[][] {
+                {2,1,0,1,0},
+                {0,2,1,0,1},
+                {1,0,2,1,0},
+                {0,1,0,2,1},
+                {1,0,1,0,2}};
+        handToInt = new HashMap<String,Integer>(){{
+            put("Scissors", 1);
+            put("Paper",2);
+            put("Rock",3);
+            put("Lizard", 4);
+            put("Spock",5);
+        }};
+    }
     
     @FXML
     public void processAction(){
+
 
     }
 }
